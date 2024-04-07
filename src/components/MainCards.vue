1a6f993da483334505234a2980395aff314825df
jmart381 <143963789+jmart381@users.noreply.github.com>
2024-04-07 14:15:47 -0500

    Initial commit

---
+++ src/components/MainCards.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainCardSingle from './MainCardSingle.vue';
+
+</script>
+
+<template>
+<div class="grid grid-cols-4 gap-8 p-10">
+    <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+</div>
+
+</template>
---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRAz_t5c3p5XZ_WwIqQRFxiwJnB0m65WE",
+  authDomain: "company-directory-a3399.firebaseapp.com",
+  projectId: "company-directory-a3399",
+  storageBucket: "company-directory-a3399.appspot.com",
+  messagingSenderId: "504924371506",
+  appId: "1:504924371506:web:3f976dc6fc2c478b70da2b"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
