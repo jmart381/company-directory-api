8ea8e470beb884ca4dd1f506f168477e18d4b440
Juan C Martinez <stc.classcontent@gmail.com>
2024-04-07 14:58:46 -0500

    connect to API is working

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,24 +1,42 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 
-const firstname = faker.person.firstName()
-const lastname = faker.person.lastName()
-const fullname = faker.person.fullName({firstName: firstname, lastName:lastname})
-const job_title = faker.person.jobTitle()
-const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+// const firstname = faker.person.firstName()
+// const lastname = faker.person.lastName()
+// const fullname = faker.person.fullName({firstName: firstname, lastName:lastname})
+// const job_title = faker.person.jobTitle()
+// const bio = faker.lorem.lines(2)
+// const username = faker.internet.userName({firstName: firstname, lastName:lastname}).toLowerCase()
+
+const props = defineProps({
+    employee: {
+        type: Object,
+        required: true,
+        default: () => {
+            return {
+                _id: "6609f865a1e0f366b02dc60a",
+                userName: "nickj",
+                firstName: "Nick",
+                lastName: "Justin",
+                title: "Student",
+                quote: "Wassaaa!!!",
+                __v: 0,
+            }
+        },
+    },
+})
 
 </script>
 
 <template>
     <!-- {{ fullname }} <br /> -->
-    <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+    <RouterLink :to="{name: 'CardDetails', params: {id: props.employee._id}}">
     <div class="border border-gray-500 rounded-lg shadow bg-gray-300 h-full">
         <img :src="faker.image.urlLoremFlickr({category: 'capybara'})" class="rounded-t-lg"/>
         <div class="p-4">
-            <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-            <p class="mb-3 font-normal text-gray-800">{{ job_title }}</p>
-            <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
+            <p class="mb-2 text-2xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ props.employee.title }}</p>
+            <p class="mb-3 font-normal text-gray-800">{{ props.employee.quote }}</p>
         </div>
     </div>
     </RouterLink>
